c13b1bcb0c8fcf9ff941a5e14d1179215f1478ab
DevSamaa <samaakanani@gmail.com>
2020-03-02 12:38:11 +1100

    first part of the incometax calculator is working

--- BasicPayslipKata/ActualApp.cs
+++ BasicPayslipKata/ActualApp.cs
@@ -5,8 +5,40 @@ namespace BasicPayslipKata
 
     class ActualApp
     {
+        public static float grossIncomeCalculator(int incomingUserAnnualSalary)
+        {
+            //Gross income = annual salary / 12 months
+            float grossIncome = incomingUserAnnualSalary / 12;
+            return grossIncome;
+        }
+
+        public static float incomeTaxCalculator(int incomingUserAnnualSalary)
+        {
+            int taxFreeAmount = 18200;
+
+            if (incomingUserAnnualSalary <= 18200)
+            {
+                return 0;
+            }
+            else if (incomingUserAnnualSalary > 18200 && incomingUserAnnualSalary < 37001)
+            {
+                //return 19;
+                int toBeTaxedAmount = incomingUserAnnualSalary - taxFreeAmount;
+                float taxBracketOneAmount = Convert.ToSingle(toBeTaxedAmount * 0.19);
+                return taxBracketOneAmount;
+
+            }
+            else
+            {
+                //this is just to test
+                return 111;
+            }
+        }
+
         public static void payslipGenerator()
         {
+
+
             Console.WriteLine("Welcome to the payslip generator!");
 
             Console.WriteLine("Please input your first name:");
@@ -19,9 +51,20 @@ namespace BasicPayslipKata
             Console.WriteLine("Please enter your annual salary:");
             int UserAnnualSalary = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Please enter your super rate:");
+            int UserSuperRate = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Your payslip has been generated:");
             Console.WriteLine($"Name: {UserFirstName} {UserLastName}");
             Console.WriteLine($"Your Annual Salary is: {UserAnnualSalary} dollars");
+            Console.WriteLine($"Your Super Rate is:{UserSuperRate}");
+
+            Console.WriteLine($"Your Gross Income is:{grossIncomeCalculator(UserAnnualSalary)} per month.");
+
+            Console.WriteLine($"Your Income Tax is:{incomeTaxCalculator(UserAnnualSalary)/12} per month.");
         }
+
 
 
     }
